0f211156b23918479bb0172a7c9df598620358ae
Rafael Villarreal <rafaell416@Earlys-MacBook-Pro.local>
2016-12-27 11:10:28 -0500

    test image completed

--- test/db-test.js
+++ test/db-test.js
@@ -26,16 +26,20 @@
    t.is(typeof db.saveImage, 'function', 'saveImage is a function')
 
    let image = {
+     description: 'an #awesome pivture with #tags #newgram',
      url: `https://newgram.test/${uuid.v4()}.jpg`,
      likes: 0,
      liked: false,
      user_id: uuid.uuid()
    }
    let created = await db.saveImage(image)
+   t.is(created.description, image.description)
    t.is(created.url, image.url)
    t.is(created.likes, image.likes)
    t.is(created.liked, image.liked)
+   t.deepEqual(created.tags, ['awesome', 'tags', 'newgram'])
    t.is(created.user_id, image.user_id)
+   t.is(created.public_id, uuid.encode(created.id))
    t.is(typeof created.id, 'string')
    t.truthy(created.createdAt)
  })
